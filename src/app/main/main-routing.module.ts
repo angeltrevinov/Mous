07403d6d66fa56ca797918cab08464f818417101
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2019-11-11 21:47:23 -0600

    Navbar styles

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {MainComponent} from './main/main.component';
+import {SearchComponent} from './main/search/search.component';
 
 const routes: Routes = [
-  { path: 'Home', component: MainComponent }
+  { path: '', component: MainComponent, children: [
+      {path: '', redirectTo: 'Search', pathMatch: 'full'},
+      {path: 'Search', component: SearchComponent }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import {MainRoutingModule} from './main-routing.module';
+import { SearchComponent } from './main/search/search.component';
 
 @NgModule({
   declarations: [
     MainComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule,
