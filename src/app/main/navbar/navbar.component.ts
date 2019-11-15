e48c45b61b8717b0aaf9999981a1c47f88f4d868
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2019-11-14 18:48:11 -0600

    search with parameters

--- src/app/main/navbar/navbar.component.ts
+++ src/app/main/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
@@ -10,12 +11,27 @@ export class NavbarComponent implements OnInit {
 
   userDetails: any;
   boolToken: boolean;
+  searchForm: FormGroup;
 
   //--------------------------------------------------------
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router
+  ) { }
 
   //--------------------------------------------------------
   ngOnInit() {
+    this.validateToken();
+    this.formCreation();
+  }
+
+  //--------------------------------------------------------
+  onLogOut() {
+    localStorage.removeItem('User');
+    this.router.navigate(['/Login']);
+  }
+
+  //--------------------------------------------------------
+  validateToken() {
     this.userDetails = JSON.parse(
       localStorage.getItem('User')
     );
@@ -27,9 +43,24 @@ export class NavbarComponent implements OnInit {
   }
 
   //--------------------------------------------------------
-  onLogOut() {
-    localStorage.removeItem('User');
-    this.router.navigate(['/Login']);
+  formCreation() {
+    this.searchForm = new FormGroup({
+      SearchTerm: new FormControl(null, {
+        validators: [
+          Validators.required
+        ]
+      })
+    });
+  }
+
+  //--------------------------------------------------------
+  get SearchTerm() { return this.searchForm.get('SearchTerm'); }
+
+  //--------------------------------------------------------
+  onSearch() {
+    if (!this.searchForm.invalid) {
+      this.router.navigate(['/Search', this.SearchTerm.value]);
+    }
   }
 
 }
