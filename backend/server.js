069de2a130ccb3cf6d18b3fa6327c871ec2e56c7
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2019-11-22 04:22:21 -0600

    trying to solve errors

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 
 
 // Start the server
-app.listen('8080', () => {
+app.listen(process.env.PORT || '8080', () => {
   console.log("App running on localhost:8080");
 });
 
