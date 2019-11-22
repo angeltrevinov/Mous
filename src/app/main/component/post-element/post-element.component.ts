cc7f321ea995b7dc9dd96c24d7bd9e5c705a5265
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2019-11-22 01:32:32 -0600

    Dar Like

--- src/app/main/component/post-element/post-element.component.ts
+++ src/app/main/component/post-element/post-element.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {PostService} from '../../../services/post.service';
 
 @Component({
   selector: 'app-post-element',
@@ -10,10 +11,31 @@ export class PostElementComponent implements OnInit {
   @Input() Post: any;
 
   //--------------------------------------------------------
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   //--------------------------------------------------------
   ngOnInit() {
+    console.log(this.Post);
+  }
+
+  //--------------------------------------------------------
+  OnLike() {
+    this.postService.LikePost(this.Post._id).subscribe((result) => {
+      this.Post.bLike = true;
+      this.Post.arrLikes++;
+    }, (error) => {
+      console.log(error);
+    });
+  }
+
+  //--------------------------------------------------------
+  OnDislike() {
+    this.postService.UnlikePost(this.Post._id).subscribe((result) => {
+      this.Post.bLike = false;
+      this.Post.arrLikes--;
+    }, (error) => {
+      console.log(error);
+    });
   }
 
 }
